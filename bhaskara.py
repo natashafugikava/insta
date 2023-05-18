886515159a5360588810fbb1db27682294480aa5
natashafugikava <113446005+natashafugikava@users.noreply.github.com>
2023-05-18 09:08:42 -0300

    Update bhaskara.py

--- bhaskara.py
+++ bhaskara.py
@@ -12,7 +12,7 @@ if delta > 0:
   x1 = (-b + delta**(1/2))/(2*a)
   x2 = (-b - delta**(1/2))/(2*a)
   st.write(f'As raízes são {x1:.2f} e {x2:.2f}.')
-elif delta = 0:
+elif delta == 0:
   x = -b/(2*a)
   st.write(f'Raiz dupla igual a {x:.2f}.')
 else:
